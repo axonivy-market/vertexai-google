e7eb57104c4120e9f606cc1903d087ca12330708
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2025-07-21 16:52:25 +0700

    MARP-1983 Update UT

--- vertexai-google-test/src_test/com/axonivy/connector/vertexai/mock/constants/VertexaiTestConstants.java
+++ vertexai-google-test/src_test/com/axonivy/connector/vertexai/mock/constants/VertexaiTestConstants.java
@@ -1,6 +1,6 @@
 package com.axonivy.connector.vertexai.mock.constants;
 
-public class VertexaiCommonConstants {
+public class VertexaiTestConstants {
   public static final String MOCK_SERVER_CONTEXT_DISPLAY_NAME = "Mock Server Test";
   public static final String REAL_CALL_CONTEXT_DISPLAY_NAME = "Real Server Test";
 }
