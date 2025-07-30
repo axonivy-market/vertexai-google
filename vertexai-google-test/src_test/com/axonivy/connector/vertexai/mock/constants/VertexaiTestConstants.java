bd8a2b7eb3a621148784d37aecbf52fad3d2d1d1
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-07-30 22:59:22 +0700

    update e2e workflow

--- vertexai-google-test/src_test/com/axonivy/connector/vertexai/mock/constants/VertexaiTestConstants.java
+++ vertexai-google-test/src_test/com/axonivy/connector/vertexai/mock/constants/VertexaiTestConstants.java
@@ -1,6 +1,8 @@
 package com.axonivy.connector.vertexai.mock.constants;
 
 public class VertexaiTestConstants {
+  public static final String END_TO_END_TESTING_ENVIRONMENT_KEY = "testEnvironment";
+  public static final String END_TO_END_TESTING_ENVIRONMENT_VALUE = "E2E";
   public static final String MOCK_SERVER_CONTEXT_DISPLAY_NAME = "Mock Server Test";
   public static final String REAL_CALL_CONTEXT_DISPLAY_NAME = "Real Server Test";
 }
