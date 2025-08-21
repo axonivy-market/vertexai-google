3faab83da8f508a393bc64392c83e347ed7ba3f1
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2025-08-21 11:42:24 +0700

    MARP-2631 Remove unused code

---
+++ vertexai-google-test/src_test/com/axonivy/connector/vertexai/BaseSetup.java
@@ -0,0 +1,29 @@
+package com.axonivy.connector.vertexai;
+
+import ch.ivyteam.ivy.environment.AppFixture;
+
+public class BaseSetup {
+  protected Runnable runRealEnv(AppFixture fixture) {
+    return () -> {
+      String vertexaiProjectId = System.getProperty("vertexaiProjectId");
+      String vertexaiLocation = System.getProperty("vertexaiLocation");
+      String vertexaiModelName = System.getProperty("vertexaiModelName");
+      String geminiApiKey = System.getProperty("geminiApiKey");
+
+      fixture.var("gemini.apiKey", geminiApiKey);
+      fixture.var("vertexaiGemini.projectId", vertexaiProjectId);
+      fixture.var("vertexaiGemini.location", vertexaiLocation);
+      fixture.var("vertexaiGemini.modelName", vertexaiModelName);
+    };
+  }
+
+  protected Runnable runMockEnv(AppFixture fixture) {
+    return () -> {
+      fixture.var("vertexaiGemini.projectId", "generate-images-for-process");
+      fixture.var("vertexaiGemini.location", "us-central");
+      fixture.var("vertexaiGemini.modelName", "gemini-1.5-pro-preview-0409");
+      fixture.var("vertexaiGemini.keyFilePath", "D:\\test.json");
+      fixture.var("gemini.apiKey", "AIzaSyDaxbn4Ragu");
+    };
+  }
+}
